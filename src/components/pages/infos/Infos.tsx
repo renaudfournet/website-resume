ae452f09be41e3b8f8bccaffb5cefd546dabc9e2
Renaud <fournet.renaud@gmail.com>
2022-06-06 11:51:38 +0200

    fixed align first card

--- src/components/pages/infos/Infos.tsx
+++ src/components/pages/infos/Infos.tsx
@@ -13,23 +13,23 @@ function Infos() {
   return !show ? (
     <div
       onMouseOver={showMore}
-      className="flex relative bg-white-100 w-80 h-80 xs:w-96 xs:h-96 md:w-96 md:h-96 rounded-lg"
+      className="flex p-10 relative bg-white-100 w-80 h-80 xs:w-96 xs:h-96 md:w-96 md:h-96 rounded-lg"
     >
-      <div className="p-10">
-        <div className="mb-10 mt-8">
-          <p className="flex text-left  text-black-100 text-1xl xs:text-1xl sm:text-1xl">
-            <img alt="location" src={location} /> &nbsp; Bayonne, FR
-          </p>
-          <hr className="line-horizontal-blue mt-6 mb-4" />
-          <p className="flex flex-col text-left font-black text-2xl xs:text-4xl sm:text-4xl text-primary-100">
-            <span className="height-span">DÉVELOPPEUR</span>
-            <span>FRONT END</span>
+      <div className="mb-10">
+        <div className="flex items-center h-16 xs:h-32 md:h-32 justify-start text-white-100 text-1xl xs:text-2xl sm:text-2xl md:text-2l">
+          <p className="flex text-left text-black-100 ">
+            <img alt="location" className="w-6" src={location} /> &nbsp; <span>Bayonne, FR</span>
           </p>
         </div>
-        <div className="flex items-center text-primary-100 absolute p-4 bottom-0 right-0 ">
-          <p className="text-xs font-bold">À propos de moi</p>&nbsp;&nbsp;
-          <FontAwesomeIcon icon={faArrowRight} />
-        </div>
+        <hr className="line-horizontal-blue mt-6 mb-4" />
+        <p className="flex flex-col text-left font-black text-2xl xs:text-4xl sm:text-4xl text-primary-100">
+          <span className="height-span">DÉVELOPPEUR</span>
+          <span>FRONT END</span>
+        </p>
+      </div>
+      <div className="flex items-center text-primary-100 absolute p-4 bottom-0 right-0 ">
+        <p className="text-xs font-bold">À propos de moi</p>&nbsp;&nbsp;
+        <FontAwesomeIcon icon={faArrowRight} />
       </div>
     </div>
   ) : (
